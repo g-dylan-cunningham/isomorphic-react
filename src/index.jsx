fd15bb7dabbee7a822bc5558f2b4081f950c172d
Dylan Cunningham <gdylanc@gmail.com>
2018-10-28 11:20:57 -0700

    hooks up App.jsx to render render in index.html

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,12 @@
-console.info('pulling in js from src/index')
+import App from './App';
+import ReactDom from 'react-dom';
+import React from 'react';
+
+const render = (_App) => {
+    ReactDom.render(
+        <_App />,
+        document.getElementById('AppContainer')
+    )
+}
+
+render(App);
